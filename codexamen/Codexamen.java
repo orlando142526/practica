61b1110faa99e233dd31ed79c1d4e0b69c14b5e5
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2019-11-28 09:46:55 +0100

    segundo commit de practica examen

--- codexamen/Codexamen.java
+++ codexamen/Codexamen.java
@@ -17,6 +17,7 @@ public class Codexamen {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("examen contornos");
+        System.out.println("adios a examen contornos");
     }
     
 }
